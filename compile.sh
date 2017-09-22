74758e024585eb7b889d058679d4dc1c51bdb320
Mathieu Fourticq-Tire <mathieu.fourticq-tire@nokia.com>
2017-09-22 14:23:20 +0300

    Remove unecessary include files

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PROJECT_BASE_DIR=$(readlink -f $(dirname $BASH_SOURCE))
+pushd ${PROJECT_BASE_DIR}
 find ${PROJECT_BASE_DIR} -name "*.o" | xargs rm
-gcc  -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -c  ${PROJECT_BASE_DIR}/crypto/md5/*.c ${PROJECT_BASE_DIR}/crypto/* ${PROJECT_BASE_DIR}/main.c
-gcc  -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -o encrypt *.o
+OPTIONS="-v -nostdinc -I/usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/ -I/local/mfourtic/DEV/EscapeGame/short/fake/"
+gcc $OPTIONS -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -c  ${PROJECT_BASE_DIR}/crypto/md5/*.c ${PROJECT_BASE_DIR}/crypto/* ${PROJECT_BASE_DIR}/main.c
+gcc $OPTIONS -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -o encrypt *.o
+popd
