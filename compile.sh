dc507e68bce7442d8d9607b54b47e0665b91d4c7
Mathieu Fourticq-Tire <mathieu.fourticq-tire@nokia.com>
2017-09-22 14:27:21 +0300

    Remove the trick to compile without openssl includes

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 PROJECT_BASE_DIR=$(readlink -f $(dirname $BASH_SOURCE))
 pushd ${PROJECT_BASE_DIR}
 find ${PROJECT_BASE_DIR} -name "*.o" | xargs rm
-OPTIONS="-v -nostdinc -I/usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/ -I/local/mfourtic/DEV/EscapeGame/short/fake/"
+OPTIONS="-v"
 gcc $OPTIONS -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -c  ${PROJECT_BASE_DIR}/crypto/md5/*.c ${PROJECT_BASE_DIR}/crypto/* ${PROJECT_BASE_DIR}/main.c
 gcc $OPTIONS -I/local/mfourtic/DEV/EscapeGame/short/include -I/local/mfourtic/DEV/EscapeGame/short/crypto/include  -g -o encrypt *.o
 popd
